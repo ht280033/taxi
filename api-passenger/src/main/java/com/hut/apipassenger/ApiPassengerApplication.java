3567176a9c4c7eb4f333f6ac49e82efa373ea1fa
胡涛 <hut@landray.com.cn>
2023-05-21 17:03:25 +0800

    验证码服务创建以及API服务测试

--- api-passenger/src/main/java/com/hut/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/hut/apipassenger/ApiPassengerApplication.java
@@ -2,8 +2,10 @@ package com.hut.apipassenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiPassengerApplication {
 
     public static void main(String[] args) {
