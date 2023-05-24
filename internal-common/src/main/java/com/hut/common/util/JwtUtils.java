d3ad05ed077d4aeb33ddf773c6e36cc44aa345cb
胡涛 <1727016217@qq.com>
2023-05-24 11:08:24 +0800

    JWT工具类token生成与解析

--- internal-common/src/main/java/com/hut/common/util/JwtUtils.java
+++ internal-common/src/main/java/com/hut/common/util/JwtUtils.java
@@ -3,6 +3,8 @@ package com.hut.common.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -13,9 +15,12 @@ public class JwtUtils {
     //盐
     private static final String SIGN = "CPFhut!@#SS";
 
+    private static final String JWT_KEY = "passengerPhone";
 
     //生成Token
-    private static String generateToken(Map<String, String> map){
+    private static String generateToken(String passengerPhone){
+        Map<String, String> map = new HashMap<>();
+        map.put(JWT_KEY, passengerPhone);
         //token过期时间
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE,1);
@@ -24,25 +29,20 @@ public class JwtUtils {
         JWTCreator.Builder builder = JWT.create();
 
         //整合map数据
-        map.forEach((key, value) -> builder.withClaim(map.get(key), value));
+        map.forEach(builder::withClaim);
         //整合过期时间
         builder.withExpiresAt(time);
 
         //生成token
-        String sign = builder.sign(Algorithm.HMAC256(SIGN));
+        String token = builder.sign(Algorithm.HMAC256(SIGN));
 
-        return sign;
+        return token;
     }
-
-    public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-
-        map.put("name","name123");
-        map.put("age","18");
-        String s = generateToken(map);
-        System.out.println(s);
-    }
-
 
     //解析Token
+    public static String parseToken(String token){
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+        return verify.getClaim(JWT_KEY).asString();
+    }
+
 }
